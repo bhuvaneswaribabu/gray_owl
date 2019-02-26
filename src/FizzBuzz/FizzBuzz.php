705cc48f7b5e92e064ff0c5420fe847eb04acbab
bhuvaneswaribabu <bhuvaneswaribabu@users.noreply.github.com>
2019-02-26 11:56:50 +0100

    Add files via upload

--- src/FizzBuzz/FizzBuzz.php
+++ src/FizzBuzz/FizzBuzz.php
@@ -1,7 +1,65 @@
 <?php
 
+
+
 namespace SDM\FizzBuzz;
 
 class FizzBuzz implements FizzBuzzInterface
 {
+    $testNumber;
+    $fizz=3;
+    $buzz=5;
+    
+                
+        public function getResults($testNumber, $fizz,  $buzz ){
+            try {
+                if ($testNumber <1 || $testNumber >100 ) {
+                throw new Exception("The number is not valid."); 
+                }
+
+                if(  !($testNumber % ($fizz*$buzz) )     ){
+                    return "FizzBuzz";
+                }
+                elseif (  !($testNumber % $buzz )     ){
+                    return "Buzz";
+                }
+                elseif(  !($testNumber % $fizz )     ){
+                    return "Fizz";
+                } 
+                else {
+                    return $testNumber;
+                }
+            }
+            catch (Exception $e) {
+                    echo "The number is not valid.";
+            }        
+
+        }
+
+        public function getResults($testNumber){
+            try {
+                if ($testNumber <1 || $testNumber >100 ) {
+                throw new Exception("The number is not valid."); 
+                }
+
+                if(  !($testNumber % ($this->fizz*$this->buzz) )     ){
+                    return "FizzBuzz";
+                }
+                elseif (  !($testNumber % $this->buzz )     ){
+                    return "Buzz";
+                }
+                elseif(  !($testNumber % $this->fizz )     ){
+                    return "Fizz";
+                } 
+                else {
+                    return $testNumber;
+                }
+            }
+            catch (Exception $e) {
+                    echo "The number is not valid.";
+            }        
+
+        }        
 }
+
+?>
